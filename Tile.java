dd5412f1f630058cbd02506b0bee267301eb8a0b
Itay7414 <itaykoko7414@gmail.com>
2024-04-30 16:46:36 +0300

    bag update

--- Tile.java
+++ Tile.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.util.Arrays;
+import java.util.Random;
 
 public class Tile {
     final public int score;
@@ -55,41 +57,66 @@ public class Tile {
                 return 0;
         }
     }
-    public class Bag {
-        int numOfTiles[];
-        int quantities[];
-        Tile letters[];
+    public static class Bag {
+        public static Bag bag = null;
+        private final int[] maxQuantites;
+        public int[] quantities;
+        private final Tile[] tiles;
+
+        private Random rand;
+        private int size;
 
         public Bag(){
-            numOfTiles = new int[]{1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,1,10};
+            maxQuantites = new int[]{1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,1,10};
+            quantities = maxQuantites.clone();
+            size = Arrays.stream(maxQuantites).sum();
+            rand = new Random();
 
-            letters = new Tile[26];
-            letters[0] = new Tile('A');
-            letters[1] = new Tile('B');
-            letters[2] = new Tile('C');
-            letters[3] = new Tile('D');
-            letters[4] = new Tile('E');
-            letters[5] = new Tile('F');
-            letters[6] = new Tile('G');
-            letters[7] = new Tile('H');
-            letters[8] = new Tile('I');
-            letters[9] = new Tile('J');
-            letters[10] = new Tile('K');
-            letters[11] = new Tile('L');
-            letters[12] = new Tile('M');
-            letters[13] = new Tile('N');
-            letters[14] = new Tile('O');
-            letters[15] = new Tile('P');
-            letters[16] = new Tile('Q');
-            letters[17] = new Tile('R');
-            letters[18] = new Tile('S');
-            letters[19] = new Tile('T');
-            letters[20] = new Tile('U');
-            letters[21] = new Tile('V');
-            letters[22] = new Tile('W');
-            letters[23] = new Tile('X');
-            letters[24] = new Tile('Y');
-            letters[25] = new Tile('Z');
+            tiles = new Tile[26];
+            tiles[0] = new Tile('A');
+            tiles[1] = new Tile('B');
+            tiles[2] = new Tile('C');
+            tiles[3] = new Tile('D');
+            tiles[4] = new Tile('E');
+            tiles[5] = new Tile('F');
+            tiles[6] = new Tile('G');
+            tiles[7] = new Tile('H');
+            tiles[8] = new Tile('I');
+            tiles[9] = new Tile('J');
+            tiles[10] = new Tile('K');
+            tiles[11] = new Tile('L');
+            tiles[12] = new Tile('M');
+            tiles[13] = new Tile('N');
+            tiles[14] = new Tile('O');
+            tiles[15] = new Tile('P');
+            tiles[16] = new Tile('Q');
+            tiles[17] = new Tile('R');
+            tiles[18] = new Tile('S');
+            tiles[19] = new Tile('T');
+            tiles[20] = new Tile('U');
+            tiles[21] = new Tile('V');
+            tiles[22] = new Tile('W');
+            tiles[23] = new Tile('X');
+            tiles[24] = new Tile('Y');
+            tiles[25] = new Tile('Z');
+        }
+
+        public getRand(){
+            
+        }
+
+        public getTile(char tile)
+        {
+
+        }
+
+        public put(char tile)
+        {
+            
+        }
+        
+        public getQuantities()
+        {
             
         }
     }
